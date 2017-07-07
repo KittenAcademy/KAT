d0af8ad39801531c1ac0d45d4b1fbc2f1f56512e
Mark Dell <madelk@gmail.com>
2017-07-06 21:07:35 -0500

    Fix line breaks

--- src/discord/module/jokewords.js
+++ src/discord/module/jokewords.js
@@ -6,8 +6,8 @@ module.exports = [
 	"Q: What is a cat's way of keeping law & order?\nA: Claw Enforcement!",
 	"Q: What is a cat's favourite movie?\nA: The Sound of Mewsic!",
 	"Q: Why did the cat put oil on the mouse?\nA: Because it squeaked.",
-	"Q: If lights run on electricity and cars run on gas, what do cats run on?/nA: Their Paws!",
-	"Q: Why did the cat run from the tree?/nA: Because it was afraid of the bark!",
-	"Q: What is a cats favourite colour?/nA: Purrrrrrple!",
-	"Q: What do you call the cat that was caught by the police?/nA: The purrpatrator."
+	"Q: If lights run on electricity and cars run on gas, what do cats run on?\nA: Their Paws!",
+	"Q: Why did the cat run from the tree?\nA: Because it was afraid of the bark!",
+	"Q: What is a cats favourite colour?\nA: Purrrrrrple!",
+	"Q: What do you call the cat that was caught by the police?\nA: The purrpatrator."
 ];
