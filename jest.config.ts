51231e5b1f6a30e1dcf0d023974926029d2ce4e5
Mark Dell <madelk@gmail.com>
2022-03-12 20:40:04 -0600

    Try a different test file

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,15 @@
-export default {
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  verbose: true,
+  testEnvironment: "node",
   moduleFileExtensions: ["ts", "tsx", "js"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  testEnvironment: "node",
   rootDir: "src",
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
 };
+
+export default config;
